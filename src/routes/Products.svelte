eb81c40a48e52abfaf077194b311cc9d82416b2d
Devshara <developer.ishara@gmail.com>
2023-08-03 07:35:59 +0530

    initial commit

---
+++ src/routes/Products.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { onMount } from "svelte";
+
+
+    let data;
+
+    onMount(() => {
+        fetchProducts()
+    })
+
+    async function fetchProducts(){
+        const res = await fetch('https://fakestoreapi.com/products');
+        data = await res.json();
+
+        console.log(data)
+    }
+    
+
+
+</script>
+
+<button on:click={() => {
+    fetchProducts()
+}}>filter</button>
