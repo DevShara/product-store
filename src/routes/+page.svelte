dd2da17e1d61a0f230158597c24e10b492253d9c
DevShara <developer.ishara@gmail.com>
2023-08-03 11:31:56 +0530

    create product grid and styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import ProductList from './ProductList.svelte';
 	import Products from './Products.svelte';
+	import '../app.css';
 </script>
 
 <h1>Welcome to Products Store</h1>
--- src/routes/ProductList.svelte
+++ src/routes/ProductList.svelte
@@ -1,12 +1,29 @@
 <script>
 	import { onMount } from 'svelte';
+	import '../app.css';
 	import { productStore } from '../stores';
 
 	$: products = $productStore;
 </script>
 
-<ul>
-	{#each products as product}
-		<li>{product.title}</li>
-	{/each}
-</ul>
+<div class=" bg-gray-300">
+	<div class=" container m-auto grid md:grid-cols-4 gap-6">
+		{#each products as product}
+			<div class=" lg:p-4 p-3 border-b-8 border-cyan-500 bg-white rounded-br-2xl">
+				<div class=" overflow-hidden h-48">
+					<img src={product.image} alt="" class=" max-w-full h-auto" />
+				</div>
+				<div class="p-3">
+					<h3 class="h-2/6 text-xl font-bold">{product.title}</h3>
+					<div class="flex lg:flex-row flex-col gap-3 justify-between mt-3">
+						<p class=" text-lg font-bold text-cyan-600">${product.price}</p>
+						<button
+							class=" bg-cyan-500 px-5 py-3 rounded-full font-bold text-white hover:bg-cyan-600 transition-all"
+							>Buy Now</button
+						>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
