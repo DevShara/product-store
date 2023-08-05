2cded776d62800797c3652985a101667f4c6ae80
Devshara <developer.ishara@gmail.com>
2023-08-05 14:28:17 +0530

    fix further filter component layout

--- src/routes/components/ProductFilter.svelte
+++ src/routes/components/ProductFilter.svelte
@@ -26,65 +26,68 @@
 </script>
 
 <div class=" border-b-4 border-cyan-500">
-	<div class="container m-auto py-6 flex flex-row justify-between text-sm w-full">
-		<div class=" w-7/12 flex">
-			<div class="w-2/5">
-				<label for="category" class="w-1/5">Category:</label>
+	<div class="container m-auto py-6 px-6 md:px-2 gap-6 flex items-center  flex-col md:flex-row text-sm w-full justify-between ">
+		<div class= "w-full md:w-3/12 flex md:block  justify-center  md:text-center ">
+			<label for="category" class="w-1/5 mr-2">Category:</label>
 
-				<select
-					id="category"
-					class="rounded-md w-3/5 "
-					bind:value={$filterStore.category}
-					on:change={handleGoto}
-				>
-					<option value="All" selected />
+			<select
+				id="category"
+				class="rounded-md  w-4/5 md:w-3/5"
+				bind:value={$filterStore.category}
+				on:change={handleGoto}
+			>
+				<option value="All" selected />
 
-					{#each categories as category}
-						<option value={category}>{category}</option>
-					{/each}
-				</select>
-			</div>
-			<div class="w-3/5">
-				<label for="price"  class="w-1/5">Price:</label>
-				<input
-					class="rounded-md w-2/6"
-					type="number"
-					id="minPrice"
-					bind:value={$filterStore.priceRange.min}
-					step="10"
-					min="0"
-					max="1000"
-					placeholder="min"
-					on:change={handleGoto}
-				/>
-				<input
-					class="rounded-md w-2/6"
-					type="number"
-					id="maxPrice"
-					bind:value={$filterStore.priceRange.max}
-					step="10"
-					min="10"
-					max="1000"
-					placeholder="max"
-					on:change={handleGoto}
-				/>
-			</div>
+				{#each categories as category}
+					<option value={category}>{category}</option>
+				{/each}
+			</select>
 		</div>
-		<div class="w-3/12">
-			
-				
-					<label for="category" class="w-1/5">Sort by:</label>
-					<select id="sort" class="rounded-md w-3/5" bind:value={$sortStore.sortBy} on:change={handleGoto}>
-					<option value="none" selected />
 
-					{#each sorts as sort}
-						<option value={sort}>{sort}</option>
-					{/each}
-				</select>
-			
+		<div class= "w-full md:w-5/12 items-center  justify-center  flex   ">
+			<label for="price" class=" mr-3 md:w-2/12 w-1/5 md:text-right ">Price: </label>
+			<div class=" flex  md:w-11/12 gap-2 w-4/5  items-center">
+				<input
+				class="rounded-md md:w-1/2 w-full"
+				type="number"
+				id="minPrice"
+				bind:value={$filterStore.priceRange.min}
+				step="10"
+				min="0"
+				max="1000"
+				placeholder="min"
+				on:change={handleGoto}
+			/>
+			<input
+				class="rounded-md md:w-1/2 w-full"
+				type="number"
+				id="maxPrice"
+				bind:value={$filterStore.priceRange.max}
+				step="10"
+				min="10"
+				max="1000"
+				placeholder="max"
+				on:change={handleGoto}
+			/>
+			</div>
 		</div>
 	
 
-		<button class="bg-cyan-500 px-3 w-1/12" on:click={resetFilters}>reset</button>
+		<div class=" w-full md:w-3/12  md:block flex md:text-center   justify-center ">
+			<label for="sort" class="w-1/5 mr-2">Sort by:</label>
+			<select id="sort" class="rounded-md md:w-4/6   w-4/5 " bind:value={$sortStore.sortBy} on:change={handleGoto}>
+				<option value="none" selected />
+
+				{#each sorts as sort}
+					<option value={sort}>{sort}</option>
+				{/each}
+			</select> 
+		</div>
+
+		<div class= "w-full md:w-1/12  md:text-center ">
+			<button class=" bg-gray-400 p-3 w-full text-md rounded-md text-white" on:click={resetFilters}
+				>reset</button
+			>
+		</div>
 	</div>
 </div>
